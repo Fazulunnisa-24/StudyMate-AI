a45f0b18eb9538ccbc8b343b33d07d6e608a1309
Meer Fazulunnisa <meernahid53@gmail.com>
2026-08-17 12:32:17 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://studymate-ai-backend-0wc2.onrender.com",
+  baseURL: "https://studymate-ai-backend-0wc2.onrender.com"
 });
